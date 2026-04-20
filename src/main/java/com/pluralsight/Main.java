dfb35cd05d16a18144a3f714c330595a126c3973
RickelviC <Cruzrickelvi@gmail.com>
2026-04-20 15:18:52 -0400

    added readers with printers for each story

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,8 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -17,9 +17,33 @@ public class Main {
             BufferedReader hanselReader = new BufferedReader(new FileReader("hansel_and_gretel.txt"));
             BufferedReader maryReader = new BufferedReader(new FileReader("mary_had_a_little_lamb.txt"));
 
-        } catch (FileNotFoundException ex) {
-            System.out.println("Something went wrong.");
-        }
+            System.out.println("Enter the name of a story: ");
+            String userinput = scanner.nextLine();
 
+            if (userinput.equalsIgnoreCase("goldilocks.txt")) {
+                String input;
+                while ((input = goldilocksReader.readLine()) != null) {
+                    System.out.println(input);
+                }
+                goldilocksReader.close();
+            } else if (userinput.equalsIgnoreCase("hansel_and_gretel.txt")) {
+                String input;
+                while ((input = hanselReader.readLine()) != null) {
+                    System.out.println(input);
+                }
+            } else if (userinput.equalsIgnoreCase("mary_had_a_little_lamb.txt")) {
+                String input;
+                while ((input = maryReader.readLine()) != null) {
+                    System.out.println(input);
+                }
+            }else {
+                System.out.println("not an option");
+            }
+
+
+        } catch (IOException ex) {
+            System.out.println("Something went wrong.");
+            scanner.close();
+        }
     }
 }
