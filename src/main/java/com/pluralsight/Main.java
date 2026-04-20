95c0d9521adfda1e5b77fed6ab01630c2dcbce49
RickelviC <Cruzrickelvi@gmail.com>
2026-04-20 14:54:20 -0400

    added scanner and reader for each story

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        try {
+
+            BufferedReader goldilocksReader = new BufferedReader(new FileReader("goldilocks.txt"));
+            BufferedReader hanselReader = new BufferedReader(new FileReader("hansel_and_gretel.txt"));
+            BufferedReader maryReader = new BufferedReader(new FileReader("mary_had_a_little_lamb.txt"));
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Something went wrong.");
+        }
+
+    }
 }
